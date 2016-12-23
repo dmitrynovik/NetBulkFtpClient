27df674b9be90ba85ba58dd5d31f3ab6ddb659e3
dmitrynovik <cryinstone@gmail.com>
2016-12-23 18:42:03 +1100

    Working

---
+++ BulkFtpClient/BulkFtpClient.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace BulkFtpClient.Ftp
+{
+    public class BulkFtpClient
+    {
+        private readonly int _bufferSize;
+
+        public BulkFtpClient(int bufferSize = 1024 * 1024)
+        {
+            _bufferSize = bufferSize;
+        }
+
+        public void DownloadDirectory(string fromUrl, string intoDirectory)
+        {
+            if (fromUrl == null) throw new ArgumentNullException(nameof(fromUrl));
+            if (intoDirectory == null) throw new ArgumentNullException(nameof(intoDirectory));
+
+            if (!fromUrl.EndsWith("/"))
+                fromUrl = fromUrl + "/";
+
+            var files = ListFiles(fromUrl);
+
+            files.Each(f =>
+            {
+                DownloadSingleFile(fromUrl + f, intoDirectory);
+            });
+        }
+
+        public void DownloadSingleFile(string fromUrl, string intoDirectory)
+        {
+            if (fromUrl == null) throw new ArgumentNullException(nameof(fromUrl));
+            if (intoDirectory == null) throw new ArgumentNullException(nameof(intoDirectory));
+
+            var fileName = new Uri(fromUrl).Segments.Last();
+            if (fileName == null)
+                throw new ArgumentException($"{fromUrl} is a directory");
+
+            var intoPath = Path.Combine(intoDirectory, fileName);
+
+            CreateDirIfNeeded(intoDirectory);
+
+            var buf = new char[_bufferSize];
+
+            var request = CreateFtpRequest(fromUrl, WebRequestMethods.Ftp.DownloadFile);
+            using (var response = (FtpWebResponse)request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    ThrowIfNull(fromUrl, responseStream);
+
+                    using (var reader = new StreamReader(responseStream))
+                    {
+                        using (var writeStream = File.OpenWrite(intoPath))
+                        {
+                            using (var writer = new StreamWriter(writeStream, reader.CurrentEncoding))
+                            {
+                                while (true)
+                                {
+                                    int readBytes = reader.Read(buf, 0, _bufferSize);
+                                    if (readBytes <= 0) break;
+
+                                    writer.Write(buf, 0, readBytes);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public ICollection<string> ListFiles(string fromUrl)
+        {
+            var request = CreateFtpRequest(fromUrl, WebRequestMethods.Ftp.ListDirectory);
+            var files = new List<string>();
+
+            using (var response = (FtpWebResponse)request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    ThrowIfNull(fromUrl, responseStream);
+
+                    using (var reader = new StreamReader(responseStream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var filename = reader.ReadLine();
+                            files.Add(filename);
+                        }
+                    }
+                }
+            }
+            return files;
+        }
+
+        private static void CreateDirIfNeeded(string intoPath)
+        {
+            if (!Directory.Exists(intoPath))
+                Directory.CreateDirectory(intoPath);
+        }
+
+        private static void ThrowIfNull(string fromUrl, Stream responseStream)
+        {
+            if (responseStream == null)
+                throw new IOException($"Could not open FTP stream from {fromUrl}");
+        }
+
+        private FtpWebRequest CreateFtpRequest(string fromUrl, string method)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(fromUrl);
+            request.Method = method;
+            return request;
+        }
+    }
+}
---
+++ BulkFtpClient/EnumerableExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BulkFtpClient.Ftp
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> items)
+        {
+            return items == null || !items.Any();
+        }
+
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> items)
+        {
+            return new HashSet<T>(items);
+        }
+
+        public static void Each<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            if (items != null && action != null)
+                foreach (var i in items)
+                    action(i);
+        }
+
+        public static void AddMany<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            if (collection == null || items == null)
+                return;
+
+            items.Each(collection.Add);
+        }
+
+        public static ICollection<T> RemoveMany<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        {
+            if (collection == null || predicate == null)
+                return collection;
+
+            return collection.Where(x => !predicate(x)).ToList();
+        }
+
+        public static TVal GetOrDefault<TKey, TVal>(this IDictionary<TKey, TVal> map, TKey key)
+        {
+            return map.ContainsKey(key) ? map[key] : default(TVal);
+        }
+    }
+}
